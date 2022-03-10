2609609545aa786dec77cd11477310240114f144
aniloli42 <aniloli42@gmail.com>
2022-03-10 23:15:10 +0545

    fix contact title color

--- components/ContactMe/ContactMe.js
+++ components/ContactMe/ContactMe.js
@@ -48,7 +48,7 @@ const ContactMe = () => {
 
   return (
     <section id='contact-me' className='my-20 scroll-m-24 py-4'>
-      <h2 className='mb-16 text-center text-3xl font-bold text-white underline underline-offset-4'>
+      <h2 className='mb-16 text-center text-3xl font-bold text-main underline underline-offset-4'>
         CONTACT ME
       </h2>
 
