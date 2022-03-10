b8d9c75b554bb8c48f5fcae3a2d24f55b4ef5897
aniloli42 <aniloli42@gmail.com>
2022-03-10 13:26:08 +0545

    add out for github pages

---
+++ out/_next/static/eFGWdyPTy1Bh-D0RcAkHc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/690-628363ece023ae96.js","static/chunks/pages/index-9dcb0ea09db040af.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
