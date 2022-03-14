c67b4078cba6446ea6f4d9b10d457f33360c0d1d
aniloli42 <aniloli42@gmail.com>
2022-03-14 23:35:59 +0545

    fix the send message

--- components/Portfolio/PortfolioType.js
+++ components/Portfolio/PortfolioType.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react'
 
 const PortfolioType = ({ type, active, toggleActive }) => {
   return (
     <button
-      className={`flex shrink-0 select-none items-center justify-center rounded-full border-2 border-main px-4 py-1 text-base text-main hover:bg-main hover:text-secondary focus-visible:bg-main focus-visible:text-secondary
+      className={`flex shrink-0 select-none items-center justify-center rounded-full border-2 border-main px-4 py-1 text-base text-main focus-visible:bg-main focus-visible:text-secondary
     ${
-      active ? "bg-main text-secondary" : "bg-secondary text-main"
+      active ? 'bg-main text-secondary' : 'bg-secondary text-main'
     } outline-none`}
       onClick={() => toggleActive(type)}
     >
       {type}
     </button>
-  );
-};
+  )
+}
 
-export default PortfolioType;
+export default PortfolioType
--- pages/api/message.js
+++ pages/api/message.js
@@ -1,38 +1,64 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer'
+import { google } from 'googleapis'
 
 export default async function message(req, res) {
-  if (req.method !== "POST") {
-    return res.json({ message: "Hi" });
+  if (req.method !== 'POST') {
+    return res.json({ message: 'Hi' })
   }
 
+  const {
+    USER_EMAIL,
+    GMAIL_CLIENT_ID,
+    GMAIL_CLIENT_SECRET,
+    EMAIL_REFRESH_TOKEN,
+    GMAIL_REDIRECT_URI
+  } = process.env
+
+  const OAuth2Client = new google.auth.OAuth2(
+    GMAIL_CLIENT_ID,
+    GMAIL_CLIENT_SECRET,
+    GMAIL_REDIRECT_URI
+  )
+
+  OAuth2Client.setCredentials({
+    refresh_token: EMAIL_REFRESH_TOKEN
+  })
+
   try {
-    const { name, email, message } = req.body;
+    const accessToken = await OAuth2Client.getAccessToken()
+
+    if (!accessToken)
+      throw new Error('Problem in server, Mail At: aniloli42@gmail.com')
+
+    const { name, email, message } = req.body
+
     const mailer = nodemailer.createTransport({
-      service: "gmail",
+      service: 'gmail',
       auth: {
-        type: "OAuth2",
-        user: process.env.USER_EMAIL,
-        clientId: process.env.GMAIL_CLIENT_ID,
-        clientSecret: process.env.GMAIL_CLIENT_SECRET,
-        refreshToken: process.env.EMAIL_REFRESH_TOKEN
+        type: 'OAuth2',
+        user: USER_EMAIL,
+        clientId: GMAIL_CLIENT_ID,
+        clientSecret: GMAIL_CLIENT_SECRET,
+        refreshToken: EMAIL_REFRESH_TOKEN,
+        accessToken
       }
-    });
+    })
 
     const mailOptions = {
       from: process.env.username,
-      to: "aniloli42@gmail.com",
+      to: 'aniloli42@gmail.com',
       subject: `Mail By ${name}`,
       text: `
       Name: ${name}
       Email: ${email}
       Message: ${message}
       `
-    };
+    }
 
-    await mailer.sendMail(mailOptions);
+    await mailer.sendMail(mailOptions)
 
-    res.status(200).json({ message: "Successfully Sent" });
+    res.status(200).json({ message: 'Successfully Sent' })
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
 }
