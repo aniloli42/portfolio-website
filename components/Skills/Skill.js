2f6dd60d7082ee7db111c19561bca272e92ee24a
aniloli42 <aniloli42@gmail.com>
2022-05-20 09:13:54 +0545

    custom shadow enhance

--- components/Skills/Skill.js
+++ components/Skills/Skill.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Skill = ({ programming }) => {
   return (
-    <div className='portfolio-shadow rounded-md bg-white py-2 px-4'>
-      <p className='select-none text-center text-lg text-black'>
+    <div className="custom-shadow rounded-md bg-white py-2 px-4">
+      <p className="select-none text-center text-lg text-black">
         {programming}
       </p>
     </div>
