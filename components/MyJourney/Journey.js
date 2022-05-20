b05f417ea6677e0df3e2b070ea1b4b45aa5f0ff5
aniloli42 <aniloli42@gmail.com>
2022-05-20 09:15:49 +0545

    change the shadow implementation in journey

--- components/MyJourney/Journey.js
+++ components/MyJourney/Journey.js
@@ -8,7 +8,7 @@ const Journey = ({
 }) => {
   return (
     <div className='relative mb-3 pl-6 shadow-sm after:absolute after:left-0 after:top-0 after:h-6 after:w-6 after:-translate-x-[55%] after:rounded-full after:border-4 after:border-main/50 after:bg-secondary after:[content:""] md:pl-8'>
-      <div className='portfolio-shadow relative rounded-md bg-secondary px-4 py-2 after:absolute after:top-0 after:left-0 after:h-0 after:w-0 after:-translate-x-full  after:translate-y-1/4 after:border-t-[8px] after:border-b-[8px] after:border-r-[10px] after:border-main/40 after:border-t-transparent after:border-b-transparent after:[content:""]'>
+      <div className='custom-shadow relative rounded-md bg-secondary px-4 py-2 after:absolute after:top-0 after:left-0 after:h-0 after:w-0 after:-translate-x-full  after:translate-y-1/4 after:border-t-[8px] after:border-b-[8px] after:border-r-[10px] after:border-main/40 after:border-t-transparent after:border-b-transparent after:[content:""]'>
         <div>
           <pre className="text-md text-sm text-main/80">
             {`${
