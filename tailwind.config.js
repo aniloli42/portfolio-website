e4fa165e41952f76d375937ba71b94ade248dc49
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2023-10-21 10:05:47 +0545

    refactor: change folder structure to src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
-	],
-	darkMode:'class',
-	theme: {
-		extend: {
-			colors: {
-				main: '#2A2725',
-				secondary: '#FEFEFF',
-			},
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        main: '#2A2725',
+        secondary: '#FEFEFF',
+      },
+    },
+  },
+  plugins: [],
 }
