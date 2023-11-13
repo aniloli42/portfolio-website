624d9ec9b02a60e67eb244040971044bd9444ca8
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2023-11-13 23:08:02 +0545

    remove: clean all files

--- src/utils/extractDateSegment.js
+++
@@ -1,7 +0,0 @@
-export const extractDateSegment = (date) => {
-  const dateSegments = date.split('-')
-  const year = +dateSegments[0]
-  const month = +dateSegments[1]
-
-  return { year, month }
-}
