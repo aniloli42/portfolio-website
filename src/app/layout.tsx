7a49c4f3a508423f673d68103bb63cca46890079
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2023-12-21 15:58:50 +0545

    chore: favicon and og added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,29 @@
 import '@/styles/globals.css'
+import { SITE_DESCRIPTION, SITE_TITLE } from '@constants/site.constant'
+
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-	title: 'Anil Oli Portfolio',
-	description:
-		'Anil Oli is a passionate software who loves to create the solutions based on requirement and market demand.',
+	metadataBase: new URL('https://aniloli42.me'),
+	title: SITE_TITLE,
+	description: SITE_DESCRIPTION,
+	icons: [
+		{
+			url: '/favicon-16x16.png',
+			rel: 'icon',
+			sizes: '16x16'
+		},
+		{
+			url: '/favicon-32x32.png',
+			rel: 'icon',
+			sizes: '32x32'
+		},
+		{
+			url: '/apple-touch-icon.png',
+			rel: 'apple-touch-icon',
+			sizes: '180x180'
+		}
+	],
 	keywords: [
 		'anil oli',
 		'aniloli42',
@@ -17,7 +36,34 @@ export const metadata: Metadata = {
 		'web scrapping',
 		'web scrapper',
 		'hire anil oli'
-	]
+	],
+	openGraph: {
+		type: 'profile',
+		countryName: 'Nepal',
+		title: SITE_TITLE,
+		siteName: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		images: [
+			{
+				url: '/anil-og.png',
+				height: 600,
+				width: 900
+			}
+		]
+	},
+	twitter: {
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		creator: '@aniloli42',
+
+		images: [
+			{
+				url: '/anil-og.png',
+				height: 600,
+				width: 900
+			}
+		]
+	}
 }
 
 export default function RootLayout({
---
+++ src/constants/site.constant.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = 'Anil Oli Portfolio'
+export const SITE_DESCRIPTION =
+	'Anil Oli is a passionate software who loves to create the solutions based on requirement and market demand.'
