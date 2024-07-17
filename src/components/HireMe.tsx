43c3fea85bbfcb0ddf13892f31ca98f642abed0c
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2024-07-18 00:19:17 +0545

    refactor: remove hire button and change email

--- src/components/HireMe.tsx
+++ src/components/HireMe.tsx
@@ -1,25 +1,15 @@
-import { SiFiverr, SiGmail, SiUpwork } from 'react-icons/si'
+import { SiGmail } from 'react-icons/si'
 import Badge from './Badge'
 import Card from './Card'
 
 const HireMe = () => {
 	return (
 		<Card className="flex flex-row flex-wrap gap-2">
-			<Badge link="https://www.upwork.com/workwith/aniloli42">
-				<SiUpwork />
-				Hire Me
-			</Badge>
-
-			<Badge link="https://www.fiverr.com/olianil">
-				<SiFiverr />
-				Hire Me
-			</Badge>
-
 			<Badge link="https://media.graphassets.com/0QgY4OCKRwKG4hRGomZt">
 				Get Resume
 			</Badge>
 
-			<Badge link="mailto:connect@aniloli42.me?subject=Hi Anil">
+			<Badge link="mailto:aniloli42@gmail.con?subject=Your Query&body=Your Message">
 				<SiGmail />
 				Send Email
 			</Badge>
